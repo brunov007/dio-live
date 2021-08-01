c968f9f594463f6a36c221d70b702bf18e7107f0
System Administrator <root@MacBook-Pro-de-Bruno.local>
2021-08-01 01:09:53 -0300

    first commit

---
+++ src/main/java/com/dio/live/model/Localidade.java
@@ -0,0 +1,20 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Localidade {
+    @Id private Long id;
+    private String descricao;
+    @ManyToOne private NivelAcesso nivelAcesso;
+}
---
+++ src/main/java/com/dio/live/service/JornadaService.java
@@ -0,0 +1,36 @@
+package com.dio.live.service;
+
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JornadaService {
+
+    @Autowired
+    JornadaRepository jornadaRepository;
+
+    public JornadaTrabalho save(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public Optional<JornadaTrabalho> getById(Long idJornada){
+        return jornadaRepository.findById(idJornada);
+    }
+
+    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> findAll(){
+        return jornadaRepository.findAll();
+    }
+
+    public void deleteJornada(Long idJornada){
+        jornadaRepository.deleteById(idJornada);
+    }
+}
